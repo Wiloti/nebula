6939268516871cea7f5700b740a0740ab911ba38
Wiloti <security@wiloti.fr>
2026-08-10 21:26:57 +0200

    glance.nix: Testing

--- modules/common/glance.nix
+++ modules/common/glance.nix
@@ -1,94 +1,62 @@
 { ... }: {
-  flake.nixosModules.commonGlance = { pkgs, lib, ... }: {
+    flake.nixosModules.commonGlance = { pkgs, lib, ... }: {
+        services.glance = {
 
-    services.glance = {
-      enable = true;
+            enable = true;
+            settings = {
+                server = {
+                    host = "0.0.0.0";
+                    port = 8080;
+                };
+                pages = [
+                    {
+                        name = "Videos";
+                        slug = "videos";
+                        desktop-navigation-width = "wide";
+                        head-widgets = [
+                            {
+                                type = "twitch-channels";
+                                channels = [
+                                    "shisheyu"
+                                    "zerator"
+                                    "otplol_"
+                                    "mistermv"
+                                    "hackthebox"
+                                    "thelaluka"
+                                    "rootme_org"
+                                ];
+                                hide-header = true;
+                                collapse-after = 1;
+                                style = "horizontal-cards";
+                            }
+                        ];
 
-      settings = {
-
-        server = {
-          host = "0.0.0.0";
-          port = 8080;
+                        columns = [
+                            {
+                                size = "full";
+                                widgets = [
+                                    {
+                                        type = "videos";
+                                        title = "凪";
+                                        limit = 6;
+                                        include-shorts = false;
+                                        style = "horizontal-cards";
+                                        channels = [ "UCbn-_A-tv1UsVYsgxbUU1Ow" ];
+                                    }
+                                    {
+                                        type = "videos";
+                                        title = "Archipel";
+                                        limit = 6;
+                                        include-shorts = false;
+                                        style = "horizontal-cards";
+                                        channels = [ "UC3zoY9LapZERsN7caDKqz0w" ];
+                                    }
+                                ];
+                            }
+                        ];
+                    }
+                ];
+            };
         };
-
-        theme = {
-          background-color = "220 16 10";
-          contrast-multiplier = 1.2;
-          primary-color = "114 139 142";
-          positive-color = "135 169 135";
-          negative-color = "196 116 110";
-        };
-
-        pages = [
-          {
-            name = "VIDEO";
-            columns = [
-              {
-                size = "full";
-                widgets = [
-                  {
-                    type = "rss";
-                    title = "Security";
-                    limit = 20;
-                    collapse-after = 5;
-                    cache = "1h";
-                    feeds = [
-                      {
-                        url = "https://feeds.feedburner.com/TheHackersNews";
-                        title = "The Hacker News";
-                      }
-                      {
-                        url = "https://www.schneier.com/feed/atom";
-                        title = "Schneier on Security";
-                      }
-                      {
-                        url = "https://portswigger.net/research/rss";
-                        title = "PortSwigger Research";
-                      }
-                      {
-                        url = "https://research.checkpoint.com/feed";
-                        title = "Check Point Research";
-                      }
-                      {
-                        url = "https://www.reddit.com/r/netsec/.rss";
-                        title = "r/netsec";
-                      }
-                    ];
-                  }
-                ];
-              }
-              {
-                size = "small";
-                widgets = [
-                  {
-                    type = "twitch-channels";
-                    channels = [
-                    ];
-                  }
-                ];
-              }
-            ];
-          }
-          {
-            name = "Videos";
-            columns = [
-              {
-                size = "full";
-                widgets = [
-                  {
-                    type = "videos";
-                    title = "YouTube";
-                    style = "grid-cards";
-                    collapse-after-rows = 3;
-                    channels = [
-                    ];
-                  }
-                ];
-              }
-            ];
-          }
-        ];
-      };
     };
-  };
 }
